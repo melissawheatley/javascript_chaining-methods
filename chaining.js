bfb71143a9aea72ad95bb77e891c6dcfe89c2c86
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-03-06 06:59:39 -0600

    removed copied instructions from js file

--- chaining.js
+++ chaining.js
@@ -1,5 +1,3 @@
-// Then output (either in the DOM or the console) the sum of all the resulting numbers.
-
 const integers = [23, 15, 6, 3, 11, 20, 18, 7, 21, 1, 29, 10, 12, 8];
 
 console.log(integers.sort(function(a, b){return a-b}).reverse().filter((value)=> value < 19).map((item) => item * 1.5 - 1).reduce((previous, current)=>previous+current));
